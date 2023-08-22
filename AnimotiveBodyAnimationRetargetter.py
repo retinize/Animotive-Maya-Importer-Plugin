6cbb92d14bd88c0d508d4ddfb2816fc05134152f
Ertan <ertan@retinize.com>
2023-08-22 10:02:57 +0100

    disabled maintain offset

--- AnimotiveBodyAnimationRetargetter.py
+++ AnimotiveBodyAnimationRetargetter.py
@@ -110,9 +110,9 @@ def create_parent_constraint(animated_children, target_children):
                 is_root = target_child == user_selected_root_bone[0]
                 constraint = None
                 if is_root:
-                    constraint = cmds.parentConstraint(animated_child, target_child, mo=True)
+                    constraint = cmds.parentConstraint(animated_child, target_child)
                 else:
-                    constraint = cmds.parentConstraint(animated_child, target_child, mo=True, st=['x', 'y', 'z'])
+                    constraint = cmds.parentConstraint(animated_child, target_child, st=['x', 'y', 'z'])
                 created_parent_constraints.append(constraint)
 
 
