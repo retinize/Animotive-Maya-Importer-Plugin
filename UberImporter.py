ca1baf15af0983053558b4bb752eb37dddd9dd93
Ertan <ertan@retinize.com>
2023-09-11 16:12:30 +0100

    cleaned script just to feel some progress :/ I'm desperate

--- UberImporter.py
+++ UberImporter.py
@@ -13,9 +13,6 @@ def get_last_composition_index():
     
     return str(max(indices) + 1)
 
-    
-
-
 def has_keyframes(node_name):
     for attr in cmds.listAttr(node_name, keyable=True) or []:
         if cmds.keyframe(node_name + '.' + attr, query=True, keyframeCount=True) > 0:
@@ -45,19 +42,13 @@ clipIndex = 0
 
 last_composition_name = 'MyComposition'+get_last_composition_index()
 cmds.timeEditorComposition(last_composition_name)
-#cmds.timeEditorTracks(trackName="Track1", path='MyComposition', addTrack=1)
+
 for child in all_children:
     if has_keyframes(child):
 
         cmds.timeEditorClip(child, track=last_composition_name+":1",clipId=clipIndex)
-        #cmds.timeEditorClip(child, track="MyComposition|Track1",animSource=True,clipId=clipIndex)
         clipIndex+=1
 
 
-
-
-#time_editor_objects = cmds.ls(type="timeEditor")
-#if time_editor_objects:
-#    time_editor_panel = time_editor_objects[0]
 
 
