c9936fb806a373061861219ac6c281a832bcab75
Ertan <ertan@retinize.com>
2023-12-08 16:21:51 +0000

    namespace fix imported

--- UberImporter.py
+++ UberImporter.py
@@ -123,7 +123,8 @@ def create_parent_constraint(animated_children, target_children):
     for animated_child in animated_children:
         animated_child_name = animated_child.split(':')[-1]
         for target_child in target_children:
-            if animated_child_name in target_child:
+            target_child_name = target_child.split(':')[-1]
+            if animated_child_name in target_child_name:
                 is_root = target_child == root_bone_selection[0]
                 is_hips = is_parent(root_bone_selection[0],target_child)
                 constraint = None
